f0254da3153740422fd425f0227f24e5877fc231
Nakatai-0322 <nakatai0322@gmail.com>
2022-05-20 18:57:48 +0900

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Nakatai Tools List" />
+    <img alt="logo" src="https://cdn.nakatai.ga/img/nakataimc.webp" />
+    <HelloWorld msg="Nakatai Tools List" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+    name: "App",
+    components: {
+        HelloWorld,
+    },
 };
 </script>
 
 <style>
+@import url("//fonts.googleapis.com/css2?family=Montserrat&display=swap");
+@import url("//fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap");
+@import url("//fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Montserrat", "Noto Sans JP", sans-serif;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,33 +2,20 @@
     <div class="hello">
         <h1>{{ msg }}</h1>
         <p>
-            For a guide and recipes on how to configure / customize this project,<br>
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+            Nakataiが作成したツールの紹介です。
         </p>
-        <h3>Installed CLI Plugins</h3>
+        <h3>乱数生成器</h3>
         <ul>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
-                    rel="noopener">babel</a></li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
-                    rel="noopener">eslint</a></li>
+            確かJavaScriptではじめて作ったツールです。<br>
+            Chance.jsというライブラリを使用しました。<br>
+            <a href="https://rannum.nakatai.ga">使ってみる</a><br>
+            <a href="https://github.com/Nakatai-0322/Check-Kun">ソースコード</a>
         </ul>
-        <h3>Essential Links</h3>
+        <h3>アクセス環境表示ツール</h3>
         <ul>
-            <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-            <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-            <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank"
-                    rel="noopener">vue-devtools</a></li>
-            <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+            IPアドレスなどを表示できます。<br>
+            <a href="https://check.nakatai.ga">使ってみる</a><br>
+            <a href="https://github.com/Nakatai-0322/Check-Kun">ソースコード</a>
         </ul>
     </div>
 </template>
